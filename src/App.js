e9c44176a0160661a0fce268fadfb1807f1950fd
Luke Gebbink <lukegebbink@gmail.com>
2022-01-15 18:07:50 -0700

    Push Changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import ToDoList from "./ToDoList";
 import { v4 as uuidv4 } from "uuid";
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPrint } from '@fortawesome/free-solid-svg-icons'
-
+import { faPrint, faSave, faPlusCircle, faEraser } from '@fortawesome/free-solid-svg-icons'
 import "./App.css";
+import "./Navbar.js"
+import Navbar from "./Navbar.js";
+
+
 
 const LOCAL_STORAGE_KEY = "todoApp.todos";
 
@@ -14,6 +17,7 @@ function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
   const todoListRef = useRef();
+  
   const handlePrint = useReactToPrint({
     content: () => todoListRef.current,
   });
@@ -55,27 +59,34 @@ function App() {
   return (
     <>
     <div className="Background">
-      <div className="RootDiv" class="card-header">
+      <Navbar/>
+      <div className="RootDiv" class="card-header containerClass">
          <h1>Grocery List</h1>
-        <div className="InputForTask" class="card-body InputTaskBarFrame">
+        <div className="InputForTask" class="card-body InputTaskBarFrame containerClass">
           <span class="CheapFixForBar">
           <input placeholder="Enter your desired foodage." className="TaskInputBar" id="FormControlLg" class="form-control" ref={todoNameRef} type="text" />
           </span>
           <span class="TaskCountText">
-           <h4>{todos.filter((todo) => !todo.complete).length} Tasks left in Your Day.</h4> 
+           <h4>{todos.filter((todo) => !todo.complete).length} Items To Go.</h4> 
           </span>
         </div>
-        <span ref={todoListRef}>
+        <span ref={todoListRef} class="containerClass">
         <ToDoList todos={todos} toggleTodo={toggleTodo}/>
         </span>
         <div>
           <div className="ButtonMenuDiv">
-          <button className="ButtonMenu" class="btn btn-primary" onClick={handleAddToDo}>Add Task</button>
-          <button className="ButtonMenu" class="btn btn-primary" onClick={AddToProfile}>Save To Profile</button>
-          <button className="ButtonMenu" class="btn btn-primary" onClick={handlePrint}>
-          <FontAwesomeIcon icon="fa-solid fa-print" />
-            Print List</button>
-          <button className="ButtonMenu" class="btn btn-primary" onClick={handleClearTodos}>Clear Completed Tasks</button>
+          <button className="ButtonMenu" class="btn btn-primary" onClick={handleAddToDo}>Add Task
+          <FontAwesomeIcon className="Icon" icon={faPlusCircle} color="white"/>
+          </button>
+          <button className="ButtonMenu" class="btn btn-primary" onClick={AddToProfile}>Save To Profile
+          <FontAwesomeIcon className="Icon" icon={faSave} color="white"/>
+          </button>
+          <button className="ButtonMenu" class="btn btn-primary" onClick={handlePrint}>Print List
+          <FontAwesomeIcon className="Icon" icon={faPrint} color="white"/>
+          </button>
+          <button className="ButtonMenu" class="btn btn-primary" onClick={handleClearTodos}>Clear Done Tasks
+          <FontAwesomeIcon className="Icon" icon={faEraser} color="white"/>
+          </button>
           </div>
         </div>
       </div>
